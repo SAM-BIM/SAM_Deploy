6f6d83e93aa5380cce37686049e1f82b4f8c2d87
Michal Dengusiak <tassimulation@gmail.com>
2025-11-24 11:44:44 +0100

    update buildall

--- BuildAll_release.bat
+++ BuildAll_release.bat
@@ -1,7 +1,8 @@
 @echo off
-set msbuildexe=C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\msbuild
+::set msbuildexe=C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\msbuild
+set msbuildexe=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe
 
 echo Building all SAM Solutions
-"%msbuildexe%" -t:Restore;Rebuild BuildAll_Release.csproj
+::"%msbuildexe%" -t:Restore;Rebuild BuildAll_Release.csproj
 
 "%msbuildexe%" -t:Restore;Rebuild BuildAll_Release_net.csproj
